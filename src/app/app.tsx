2c49c164c44f0dd9f335601130c168914b460054
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-05-08 16:39:56 +0300

    Add base layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,23 @@
 import styled from 'styled-components';
 
-import NxWelcome from './nx-welcome';
+import SideBar from 'src/components/side-bar/side-bar';
+import CustomersPage from 'src/components/pages/customers-page/customers-page';
+import PageContainer from 'src/components/common/page-container/page-container';
 
 const StyledApp = styled.div`
-  // Your style here
+  min-height: 100dvh;
+  min-height: 100vh;
+  display: flex;
 `;
 
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="crm-dashboard-test" />
+      <SideBar />
+
+      <PageContainer>
+        <CustomersPage />
+      </PageContainer>
     </StyledApp>
   );
 }
